0249a452be8a36378d1276f2352dbea0884ec98f
Andrei Sulimov <44847478+weterperm@users.noreply.github.com>
2024-02-29 15:05:45 +0300

    Delete S2_T1/src/cmake-build-debug directory
    cmake-build-debug directory deleted

--- S2_T1/src/cmake-build-debug/Unit_Tests/lib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/alicee/Desktop/конспекты/Современное_программирование/S2_T1/src/Unit_Tests/lib
-# Build directory: /Users/alicee/Desktop/конспекты/Современное_программирование/S2_T1/src/cmake-build-debug/Unit_Tests/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
