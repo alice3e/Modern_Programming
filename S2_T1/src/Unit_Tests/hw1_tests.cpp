2745e31becc7f6e25eae9f19ab306c7a40f33b20
Andrei Sulimov <avega2003@yandex.ru>
2024-02-29 15:01:07 +0300

    TESTS COMPLETED!

--- S2_T1/src/Unit_Tests/hw1_tests.cpp
+++ S2_T1/src/Unit_Tests/hw1_tests.cpp
@@ -1,10 +1,42 @@
 #include <gtest/gtest.h>
 #include "intr_sort.h"
 
-TEST(NAME_OF_GROUP_TEST, test1){
-std::vector <int> original{};
-std::vector <int> copy_of_original_for_std = original;
-original = IntrSort::srt(original);
-std::sort(copy_of_original_for_std.begin(),copy_of_original_for_std.end());
-ASSERT_EQ(original,copy_of_original_for_std) << "test 1 (zero size) wrong!";
+// LOAD MAIN CMAKE FILE (IN SRC DIR)
+
+TEST(Bubble_Tests, test1){
+std::vector <int> original{1};
+std::vector<int> answ;
+IntrSort::srt(answ,original);
+std::sort(original.begin(),original.end());
+ASSERT_EQ(answ,original) << "test 1 (1 element size) wrong!";
+}
+
+
+TEST(Bubble_Tests, test2){
+    std::vector <int> original{3,2,1};
+    std::vector<int> answ;
+    IntrSort::srt(answ,original);
+    std::sort(original.begin(),original.end());
+    ASSERT_EQ(answ,original) << "test 2 (3 element size) wrong!";
+}
+
+TEST(Bubble_Tests, test3){
+    std::vector <int> original{};
+    std::vector<int> answ;
+    IntrSort::srt(answ,original);
+    // std::sort(original.begin(),original.end());
+    ASSERT_EQ(answ, original) << "test 3 (zero size) wrong!";
+}
+
+TEST(Bubble_Tests, WRONG_TEST){ // SHOULD GIVE AN ERROR
+    std::vector <int> original{1,2,5,-200,5,5,0,-100};
+    std::vector<int> answ;
+    IntrSort::srt(answ,original);
+    std::sort(original.begin(),original.end());
+    ASSERT_EQ(answ,original) << "test 4 (WRONG TEST) wrong!";
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
--- S2_T1/src/hw1_insertion_sort/src/intr_sort.cpp
+++ S2_T1/src/hw1_insertion_sort/src/intr_sort.cpp
@@ -3,29 +3,35 @@
 #include <iostream>
 
 
-
-void IntrSort::srt(std::vector<int> &ans,std::vector<int> &v){ //
+void IntrSort::srt(std::vector<int> &ans, std::vector<int> &v) {
     int sz = v.size();
+    if(sz == 0){ return ; }
     ans.push_back(v[0]);
-    for(int i = 1;i<sz;i++){
-        int &el = v[i];
-        if((i+1)%2==0){ //ИНДЕКСАЦИЯ С 1 НАЧИНАЕТСЯ
-            ans.push_back(el);
-            if (el < ans[i-1]){
-                std::swap(ans[i],ans[i-1]);
+    for (int i = 1; i < sz; i++) {
+        int el = v[i];
+        ans.push_back(el);
+        int j = i;
+        bool swapped = false;
+        if (i % 2 == 1) {
+            while (j > 0 && el < ans[j - 1]) {
+                std::swap(ans[j], ans[j - 1]);
+                j--;
+                swapped = true;
             }
         }
-        else{
-            ans.push_back(el);
-            if(el > ans[i-1]){
-                std::swap(ans[i],ans[i-1]);
+        else {
+            while (j > 0 && el > ans[j - 1]) {
+                std::swap(ans[j], ans[j - 1]);
+                j--;
+                swapped = true;
+            }
+        }
+        if (!swapped) {
+            for (int k = i; k > 0; k--) {
+                if (el < ans[k - 1]) {
+                    std::swap(ans[k], ans[k - 1]);
+                }
             }
         }
     }
-
 }
-
-
-/*
-
-    */
