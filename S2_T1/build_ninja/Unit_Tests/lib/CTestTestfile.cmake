5eedddb0ad92cda9e678991720af67b0c3b047f2
Andrei Sulimov <44847478+weterperm@users.noreply.github.com>
2024-03-02 11:46:54 +0300

    Delete S2_T1/build_ninja directory

--- S2_T1/build_ninja/Unit_Tests/lib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/alicee/Desktop/конспекты/Современное_программирование/S2_T1/src/Unit_Tests/lib
-# Build directory: /Users/alicee/Desktop/конспекты/Современное_программирование/S2_T1/build_ninja/Unit_Tests/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- S2_T1/build_ninja/hw1_insertion_sort/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/alicee/Desktop/конспекты/Современное_программирование/S2_T1/src/hw1_insertion_sort/src
-# Build directory: /Users/alicee/Desktop/конспекты/Современное_программирование/S2_T1/build_ninja/hw1_insertion_sort/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
